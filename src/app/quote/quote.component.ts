a041f7fdad6576e3a4c156346d8159271108ba26
markdan58 <mark.dan@strathmore.edu>
2018-10-22 10:17:42 +0300

    added upvote,downvote function and delete function

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -6,3 +6,33 @@ import { QuotesList } from '../quotes';
     styleUrls: ['../app.component.css']
 })
 export class QuoteComponent implements OnInit {
+    quotes = QuotesList;
+
+    upvoteQuote(id) {
+        this.quotes[id - 1].quoteLikes += 1;
+    }
+
+    downvoteQuote(id) {
+        this.quotes[id - 1].quoteDislikes += 1;
+    }
+
+    toggleDetail(id) {
+        this.quotes[id - 1].detailsVisible = !this.quotes[id].detailsVisible;
+    }
+
+    QuoteDelete(isComplete, index) {
+        if (isComplete) {
+            let toDelete = confirm(`Are you sure you want to delete ${this.quotes[index].description}`);
+
+            if (toDelete) {
+                this.quotes.splice(index, 1)
+            }
+        }
+    }
+
+    constructor() { }
+
+    ngOnInit() {
+    }
+
+}

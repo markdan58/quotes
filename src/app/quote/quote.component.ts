f2bb0878aa48afd80c8b2c6e37e0cc3a323b6843
markdan58 <mark.dan@strathmore.edu>
2018-10-22 10:16:37 +0300

    added import of QouteList

---
+++ src/app/quote/quote.component.ts
@@ -0,0 +1,8 @@
+import { Component, OnInit } from '@angular/core';
+import { QuotesList } from '../quotes';
+@Component({
+    selector: 'app-quote',
+    templateUrl: './quote.component.html',
+    styleUrls: ['../app.component.css']
+})
+export class QuoteComponent implements OnInit {
